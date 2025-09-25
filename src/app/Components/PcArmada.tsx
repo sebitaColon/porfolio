460bc4456133fc39690f7576e041cb1fe2e6e214
sebitaDev <sebita03082003@gmail.com>
2025-09-24 23:29:55 -0300

    refactor: migrate project to new folder structure

---
+++ src/app/Components/PcArmada.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Monitor from "./UI/Monitor";
+import Teclado from "./UI/Teclado";
+import Image from "next/image";
+import github from '../assets/github.svg'
+import ts from '../assets/ts.svg'
+import css from '../assets/css.svg'
+import html from '../assets/html.svg'
+import js from '../assets/js.svg'
+import postgresql from '../assets/postgresql.svg'
+import react from '../assets/react.svg'
+import tailwindcss from '../assets/tailwindcss.svg'
+import Mouse from "./UI/Mouse";
+import mousepad from "../assets/fondoMousePad.jpeg"
+
+export default function PcArmada() {
+  return (
+    <div className="w-full h-[700px] relative overflow-hidden lg:overflow-visible ">
+      <div className="w-auto h-full flex absolute top-[16.6rem] left-32 sm:left-72 justify-center items-center md:top-52 md:left-[45%] lg:left-72">
+        <div className="w-[400px] h-5 bg-[#8B4513] relative" style={{ transform: "skewY(-20deg)" }}>
+          <Image src={mousepad} alt="icon" className="w-90 border-2 border-white h-54 absolute z-10 bottom-8 -left-24" style={{ transform: "skewX(45deg)" }}/>
+          <h1 className='absolute w-90 h-54 text-white font-bold flex gap-2 items-end pb-5 p-2 justify-center z-50 bottom-18 -left-6' style={{ transform: "skewX(45deg)" }}>MY SKILLS</h1>
+          <div className='absolute w-90 h-54 flex gap-2 items-end pb-5 p-2 justify-center z-50 bottom-8 -left-24' style={{ transform: "skewX(45deg)" }} >
+            <Image src={github} alt="icon" width={30}/>
+            <Image src={ts} alt="icon" width={30}/>
+            <Image src={css} alt="icon" width={30}/>
+            <Image src={html} alt="icon" width={30}/>
+            <Image src={js} alt="icon" width={30}/>
+            <Image src={postgresql} alt="icon" width={30}/>
+            <Image src={react} alt="icon" width={30}/>
+            <Image src={tailwindcss} alt="icon" width={30}/>
+          </div>
+          <div className="h-[300px] w-[400px] bg-[#a15824] bottom-full absolute" style={{ transformOrigin: "bottom", transform: "skewX(45deg)" }} ></div>
+          <div className="h-full w-[300px] bg-[#66320e] -left-[300px] top-0 absolute" style={{ transformOrigin: "right", transform: "skewY(45deg)" }} ></div>
+        </div>
+        <div className="w-auto h-auto flex -left-36 md:-left-42 -top-72 absolute ">
+          <Monitor />
+        </div>
+        <div className="w-auto h-auto flex -left-8 top-74 justify-start  items-start absolute ">
+          <Teclado />
+        </div>
+        <div className="w-auto h-auto flex left-44 top-54 justify-start  items-start absolute ">
+            <Mouse />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/Components/UI/Teclado.tsx
@@ -0,0 +1,88 @@
+"use client";
+import React, { useState, useEffect } from "react";
+const customOrder = [0, 11, 1, 2, 11, 3, 4, 5, 1, 3, 6, 0, 1, 7, 11, 8, 9, 1, 8, 10, 7, 11, 20, 20, 20, 20, 20, 12, 12, 12, 12];
+
+
+type Tecla = {
+  key?: number;
+  span?: number;
+  w?: string;
+  empty?: boolean;
+};
+const rows: Tecla[][] = [
+  [{ w: "35%", span: 2 }, {}, {}, {}, { w: "45%", span: 2 }, {}, {}, {}, {}, {}],
+  [{}, {}, {}, { key: 4 }, { key: 6 }, { key: 0 }, { key: 10 }, { key: 12, span: 2, w: "90%" }, {}],
+  [{ w: "90%", span: 2 }, { key: 1 }, { key: 3 }, {}, { key: 9 }, {}, {}, {}, {}, {}],
+  [{}, {}, { key: 8 }, { key: 5 }, { key: 7 }, { key: 2 }, {empty: true}, {empty: true}, { key: 30 }, {empty: true}],
+  [{}, {}, { key: 11, span: 3 }, {}, {empty: true}, {}, {}, {}, {}, {}]
+];
+
+export default function Teclado() {
+
+  const [isPressed, setIsPressed] = useState(Array(13).fill(false));
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(true);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout | null = null;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds + 1);
+        setIsPressed((prev) => {
+          const newPressed = Array(13).fill(false);
+          newPressed[customOrder[currentIndex]] = true;
+          return newPressed;
+        });
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % customOrder.length);
+      }, 480);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval!);
+    }
+
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, [isActive, seconds, currentIndex]);
+
+  return (
+    <div
+      className="w-[200px] h-3 bg-slate-500 relative"
+      style={{ transform: "skewY(-20deg)" }}
+    >
+      <div
+        className="h-[100px] w-full bg-slate-500 bottom-full absolute"
+        style={{ transformOrigin: "bottom", transform: "skewX(45deg)" }}
+      >
+        <div className="grid w-full h-full bg-slate-500 grid-rows-5 gap-y-1 p-1">
+          {rows.map((row, i) => (
+            <div
+              key={i}
+              className={`w-full h-full grid gap-x-1 grid-rows-1 grid-cols-${row.length}`}
+            >
+              {row.map((keyObj: Tecla, j) => {
+                if (keyObj.empty) return <div key={j}></div>; 
+                const isActiveKey = keyObj.key !== undefined ? isPressed[keyObj.key] : false;
+                const colSpan = keyObj.span ? `col-span-${keyObj.span}` : "";
+                const width = keyObj.w ? `w-[${keyObj.w}]` : "";
+                return (
+                  <div
+                    key={j}
+                    className={`teclas ${isActiveKey ? "active" : ""} ${colSpan} ${width}`}
+                  />
+                );
+              })}
+            </div>
+          ))}
+        </div>{" "}
+      </div>
+      <div
+        className="h-full w-[100px] bg-slate-700 -left-[100px] top-0 absolute"
+        style={{ transformOrigin: "right", transform: "skewY(45deg)" }}
+      ></div>
+    </div>
+  );
+}
